bf7e0772a74c556c44aaef09648ea63fc93295a6
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-06-03 02:11:24 +0100

    Adds patch to use CCFileClass instead of MixFileClass when loading VQA files.

--- src/bugfix/bugfix_hooks.cpp
+++ src/bugfix/bugfix_hooks.cpp
@@ -53,6 +53,64 @@
 
 
 /**
+ *  #issue-87
+ * 
+ *  Patch to use CCFileClass instead of MixFileClass when loading VQA files. This
+ *  allows VQA files to be loaded from the games root directory.
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_VQA_Mix_File_Handler_Use_CCFileClass_Patch)
+{
+    GET_REGISTER_STATIC(VQAClass *, this_ptr, esi);
+    GET_STACK_STATIC(char *, filename, esp, 0xC);
+
+    static int error;
+
+    /**
+     *  Original code used MixFileClass::Offset to find the file, this limited
+     *  the VQA file streamer to only be able to load files from mix files.
+     */
+#if 0
+    static MFCC *mixfile;
+    static long offset;
+    if (!MFCC::Offset(this_ptr->Filename, nullptr, &mixfile, &offset)) {
+        error = 1;
+    } else {
+        this_ptr->field_64 = this_ptr->File.Open(mixfile->Filename, FILE_ACCESS_READ);
+        error = (this_ptr->File.Seek(offset, FILE_SEEK_CURRENT) == 0);
+    }
+#endif
+
+    /**
+     *  ...Now we use CCFileClass, which does use MixFileClass to search for
+     *  the file, but also scans for the file locally first.
+     */
+    this_ptr->File.Set_Name(filename);
+    if (this_ptr->File.Is_Available()) {
+        error = 1;
+        goto exit_label;
+    }
+
+    this_ptr->field_64 = this_ptr->File.Open(FILE_ACCESS_READ);
+
+    error = !this_ptr->field_64;
+
+exit_label:
+    _asm { cmp error, 0 }
+    _asm { xor eax, eax }
+    _asm { setnz al }
+    _asm { pop esi }
+    _asm { ret 0x0C }
+}
+
+static void _Load_VQA_Locally_Patchies()
+{
+    Patch_Jump(0x0066C0FD, _VQA_Mix_File_Handler_Use_CCFileClass_Patch);
+}
+
+
+/**
  *  #issue-305
  * 
  *  Fixes bug where the sidebar mouse wheel scrolling "error" sound
@@ -621,4 +679,5 @@ void BugFix_Hooks()
     _MultiMission_Constructor_MaxPlayers_Typo_Patch();
     _OptionsClass_Constructor_IsScoreShuffle_Default_Patch();
     _Scroll_Sidebar_InGame_Check_Patch();
+    _Load_VQA_Locally_Patches();
 }
