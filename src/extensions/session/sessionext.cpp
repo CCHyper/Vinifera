8915d57f828b0424a2d5769ab7652ade35651abc
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-06-12 19:02:07 +0100

    save skirmish values.

--- src/extensions/session/sessionext.cpp
+++ src/extensions/session/sessionext.cpp
@@ -28,11 +28,10 @@
 #include "sessionext.h"
 #include "tibsun_globals.h"
 #include "noinit.h"
-#include "options.h"
+#include "session.h"
 #include "ccini.h"
 #include "rawfile.h"
-#include "voc.h"
-#include "rules.h"
+#include "language.h"
 #include "asserthandler.h"
 #include "debughandler.h"
 
@@ -51,6 +50,26 @@ SessionClassExtension::SessionClassExtension(SessionClass *this_ptr) :
     ASSERT(ThisPtr != nullptr);
     //EXT_DEBUG_TRACE("SessionClassExtension constructor - 0x%08X\n", (uintptr_t)(ThisPtr));
     //EXT_DEBUG_WARNING("SessionClassExtension constructor - 0x%08X\n", (uintptr_t)(ThisPtr));
+
+    /**
+     *  Initialise the default Skirmish options.
+     */
+    std::strcpy(SkirmishOptions.Handle, Text_String(TXT_NONAME));
+    SkirmishOptions.Side = HOUSE_FIRST;
+    SkirmishOptions.Color = PCOLOR_FIRST;
+    SkirmishOptions.Credits = 10000;
+    SkirmishOptions.GameSpeed = 3;
+    SkirmishOptions.UnitCount = 10;
+    SkirmishOptions.AIPlayers = 1;
+    SkirmishOptions.AIDifficulty = 1;
+    SkirmishOptions.Bases = true;
+    SkirmishOptions.BridgeDestruction = true;
+    SkirmishOptions.Goodies = true;
+    SkirmishOptions.ShortGame = false;
+    SkirmishOptions.CrapEngineers = false;
+    SkirmishOptions.FogOfWar = false;
+    SkirmishOptions.RedeployMCV = false;
+    SkirmishOptions.ScenarioIndex = 0;
 
     IsInitialized = true;
 }
@@ -106,6 +125,63 @@ void SessionClassExtension::Read_MultiPlayer_Settings()
     ASSERT(ThisPtr != nullptr);
     //EXT_DEBUG_TRACE("SessionClassExtension::Read_MultiPlayer_Settings - 0x%08X\n", (uintptr_t)(ThisPtr));
     //EXT_DEBUG_WARNING("SessionClassExtension::Read_MultiPlayer_Settings - 0x%08X\n", (uintptr_t)(ThisPtr));
+
+    /**
+     *  Copy the current settings to the Skirmish options. This ensures
+     *  all values are initialised if they are not found in the INI.
+     */
+    std::strcpy(SkirmishOptions.Handle, ThisPtr->Handle);
+    SkirmishOptions.Side = HOUSE_FIRST;
+    SkirmishOptions.Color = PCOLOR_FIRST;
+    SkirmishOptions.Credits = ThisPtr->Options.Credits;
+    SkirmishOptions.GameSpeed = ThisPtr->Options.GameSpeed;
+    SkirmishOptions.UnitCount = ThisPtr->Options.UnitCount;
+    SkirmishOptions.AIPlayers = ThisPtr->Options.AIPlayers;
+    SkirmishOptions.AIDifficulty = ThisPtr->Options.AIDifficulty;
+    SkirmishOptions.Bases = ThisPtr->Options.Bases;
+    SkirmishOptions.BridgeDestruction = ThisPtr->Options.BridgeDestruction;
+    SkirmishOptions.Goodies = ThisPtr->Options.Goodies;
+    SkirmishOptions.ShortGame = ThisPtr->Options.ShortGame;
+    SkirmishOptions.CrapEngineers = ThisPtr->Options.CrapEngineers;
+    SkirmishOptions.FogOfWar = ThisPtr->Options.FogOfWar;
+    SkirmishOptions.RedeployMCV = ThisPtr->Options.RedeployMCV;
+    SkirmishOptions.ScenarioIndex = ThisPtr->Options.ScenarioIndex;
+
+    /**
+     *  Read the skirmish settings.
+     */
+    ConfigINI.Get_String("Skirmish", "Handle", SkirmishOptions.Handle, sizeof(SkirmishOptions.Handle));
+    ConfigINI.Get_HousesType("Skirmish", "Side", SkirmishOptions.Side);
+    ConfigINI.Get_Int("Skirmish", "Color", SkirmishOptions.Color);
+    ConfigINI.Get_Int("Skirmish", "Credits", SkirmishOptions.Credits);
+    ConfigINI.Get_Int("Skirmish", "GameSpeed", SkirmishOptions.GameSpeed);
+    ConfigINI.Get_Int("Skirmish", "UnitCount", SkirmishOptions.UnitCount);
+    ConfigINI.Get_Int("Skirmish", "AIPlayers", SkirmishOptions.AIPlayers);
+    ConfigINI.Get_Int("Skirmish", "AIDifficulty", SkirmishOptions.AIDifficulty);
+    ConfigINI.Get_Bool("Skirmish", "Bases", SkirmishOptions.Bases);
+    ConfigINI.Get_Bool("Skirmish", "BridgeDestruction", SkirmishOptions.BridgeDestruction);
+    ConfigINI.Get_Bool("Skirmish", "Goodies", SkirmishOptions.Goodies);
+    ConfigINI.Get_Bool("Skirmish", "ShortGame", SkirmishOptions.ShortGame);
+    ConfigINI.Get_Bool("Skirmish", "MultiEngineer", SkirmishOptions.CrapEngineers);
+    ConfigINI.Get_Bool("Skirmish", "FogOfWar", SkirmishOptions.FogOfWar);
+    ConfigINI.Get_Bool("Skirmish", "RedeployMCV", SkirmishOptions.RedeployMCV);
+    ConfigINI.Get_Int("Skirmish", "ScenarioIndex", SkirmishOptions.ScenarioIndex);
+
+    SkirmishOptions.AIPlayers = std::clamp(SkirmishOptions.AIPlayers, 1, (MAX_PLAYERS-1));
+
+    ThisPtr->Options.Credits = SkirmishOptions.Credits;
+    ThisPtr->Options.GameSpeed = SkirmishOptions.GameSpeed;
+    ThisPtr->Options.UnitCount = SkirmishOptions.UnitCount;
+    ThisPtr->Options.AIPlayers = SkirmishOptions.AIPlayers;
+    ThisPtr->Options.AIDifficulty = SkirmishOptions.AIDifficulty;
+    ThisPtr->Options.Bases = SkirmishOptions.Bases;
+    ThisPtr->Options.BridgeDestruction = SkirmishOptions.BridgeDestruction;
+    ThisPtr->Options.Goodies = SkirmishOptions.Goodies;
+    ThisPtr->Options.ShortGame = SkirmishOptions.ShortGame;
+    ThisPtr->Options.CrapEngineers = SkirmishOptions.CrapEngineers;
+    ThisPtr->Options.FogOfWar = SkirmishOptions.FogOfWar;
+    ThisPtr->Options.RedeployMCV = SkirmishOptions.RedeployMCV;
+    ThisPtr->Options.ScenarioIndex = SkirmishOptions.ScenarioIndex;
 }
 
 
@@ -119,4 +195,30 @@ void SessionClassExtension::Write_MultiPlayer_Settings()
     ASSERT(ThisPtr != nullptr);
     //EXT_DEBUG_TRACE("SessionClassExtension::Write_MultiPlayer_Settings - 0x%08X\n", (uintptr_t)(ThisPtr));
     //EXT_DEBUG_WARNING("SessionClassExtension::Write_MultiPlayer_Settings - 0x%08X\n", (uintptr_t)(ThisPtr));
+
+    RawFileClass file("SUN.INI");
+
+    SkirmishOptions.AIPlayers = std::clamp(SkirmishOptions.AIPlayers, 1, (MAX_PLAYERS-1));
+
+    /**
+     *  Write the skirmish settings.
+     */
+    ConfigINI.Put_String("Skirmish", "Handle", SkirmishOptions.Handle);
+    ConfigINI.Put_HousesType("Skirmish", "Side", SkirmishOptions.Side);
+    ConfigINI.Put_Int("Skirmish", "Color", SkirmishOptions.Color);
+    ConfigINI.Put_Int("Skirmish", "Credits", SkirmishOptions.Credits);
+    ConfigINI.Put_Int("Skirmish", "GameSpeed", SkirmishOptions.GameSpeed);
+    ConfigINI.Put_Int("Skirmish", "UnitCount", SkirmishOptions.UnitCount);
+    ConfigINI.Put_Int("Skirmish", "AIPlayers", SkirmishOptions.AIPlayers);
+    ConfigINI.Put_Int("Skirmish", "AIDifficulty", SkirmishOptions.AIDifficulty);
+    ConfigINI.Put_Bool("Skirmish", "Bases", SkirmishOptions.Bases);
+    ConfigINI.Put_Bool("Skirmish", "BridgeDestruction", SkirmishOptions.BridgeDestruction);
+    ConfigINI.Put_Bool("Skirmish", "Goodies", SkirmishOptions.Goodies);
+    ConfigINI.Put_Bool("Skirmish", "ShortGame", SkirmishOptions.ShortGame);
+    ConfigINI.Put_Bool("Skirmish", "MultiEngineer", SkirmishOptions.CrapEngineers);
+    ConfigINI.Put_Bool("Skirmish", "FogOfWar", SkirmishOptions.FogOfWar);
+    ConfigINI.Put_Bool("Skirmish", "RedeployMCV", SkirmishOptions.RedeployMCV);
+    ConfigINI.Put_Int("Skirmish", "ScenarioIndex", SkirmishOptions.ScenarioIndex);
+
+    ConfigINI.Save(file, false);
 }
