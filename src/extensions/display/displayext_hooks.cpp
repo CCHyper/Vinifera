6a80d85ad429c884dc83997aceeca17263183f63
CCHyper <73803386+CCHyper@users.noreply.github.com>
2022-04-30 12:45:12 +0100

    Adds LAYER_UNDERGROUND to Next and Previous display search.

--- src/extensions/display/displayext_hooks.cpp
+++ src/extensions/display/displayext_hooks.cpp
@@ -76,6 +76,14 @@ class DisplayClassExt final : public DisplayClass
 ObjectClass * DisplayClassExt::_Next_Object(ObjectClass * object) const
 {
     static const LayerType _layers[] = {
+        
+        /**
+         *  #issue-785
+         * 
+         *  Adds underground layer to display search.
+         */
+        LAYER_UNDERGROUND,
+
         LAYER_GROUND, LAYER_AIR, LAYER_TOP,
     };
 
@@ -117,6 +125,13 @@ ObjectClass * DisplayClassExt::_Prev_Object(ObjectClass * object)  const
 {
     static const LayerType _layers[] = {
         LAYER_TOP, LAYER_AIR, LAYER_GROUND,
+
+        /**
+         *  #issue-785
+         * 
+         *  Adds underground layer to display search.
+         */
+        LAYER_UNDERGROUND,
     };
 
     ObjectClass * firstobj = nullptr;
